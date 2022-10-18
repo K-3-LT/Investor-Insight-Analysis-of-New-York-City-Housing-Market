4eed21fe55e3c198322723d1dae7464dd68c7734
K-3-LT <76082878+K-3-LT@users.noreply.github.com>
2022-10-18 10:05:40 -0400

    Create question2.sql
    Question 2
    How does a building’s age affect its selling price in relation to its building class? (Are there building classes with older/younger age enjoying higher selling prices?)

---
+++ question2.sql
@@ -0,0 +1,10 @@
+#Question 2
+#How does a building’s age affect its selling price in relation to its building class? (Are there building classes with older/younger age enjoying higher selling prices?) 
+
+SELECT (EXTRACT(YEAR FROM CURRENT_DATE()) - CAST(YEAR_BUILT AS INT)) AS building_age, 
+        ROUND(AVG(SALE_PRICE/GROSS_SQUARE_FEET)) AS price_per_square
+FROM ba775-teamproject-b1.teamproject.processed_data 
+GROUP BY building_age
+ORDER BY building_age, price_per_square DESC
+
+#A line graph can be drawn from here between building age and average price per square feet
